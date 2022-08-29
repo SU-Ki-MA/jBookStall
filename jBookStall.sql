4ab67285b14f9f7f7d2e65dbe5add9ac142ba556
SIVABALAN_SUKIMA <60312363+SU-Ki-MA@users.noreply.github.com>
2022-08-29 14:53:54 +0530

    Modify sql schema, update package.json (#3)

--- jBookStall.sql
+++ jBookStall.sql
@@ -1,22 +1,19 @@
-
-
 create database jb;
 use jb;
 
 create table authors (
 a_id varchar(50) primary key, 
 a_name varchar(50) not null, 
-a_copies_sold int(11) not null);
+a_copies_sold int not null);
 
 create table books (
 b_id varchar(11) primary key,
  b_name varchar(50) not null,
- b_price DOUBLE(10,2) not null,
- b_stock int(11) not null,
- b_copy_sold int(11) not null,
+ b_price DOUBLE not null,
+ b_stock int not null,
+ b_copy_sold int not null,
  a_id varchar(50) not null,
  pages int not null,
- book_year year,
  FOREIGN KEY (a_id) REFERENCES authors(a_id));
  
 create table users(
@@ -28,7 +25,7 @@ c_id varchar(50) not null UNIQUE,creation_time datetime,
 last_access_time datetime,
 session_creation datetime,
 session_ip_address varchar(50) not null,
-session_id varchar(50) not null); 
+session_id text not null); 
  
 
 create table carts( c_id varchar(50) primary key, 
@@ -41,7 +38,7 @@ FOREIGN KEY (u_id) REFERENCES users(u_id));
 ALTER TABLE users ADD FOREIGN KEY (email) REFERENCES email(email);
  
  create table checkouts(bill_id varchar(50) primary key ,
- bill_total DOUBLE(10,2) not null ,
+ bill_total DOUBLE not null ,
  cc_no varchar(25) , cc_name varchar(35), 
  cc_cvv varchar(5), cc_exp DATE, c_id varchar(50),
  FOREIGN KEY (c_id) REFERENCES carts(c_id));
@@ -49,8 +46,8 @@ ALTER TABLE users ADD FOREIGN KEY (email) REFERENCES email(email);
  create table cart_data(
  c_id varchar(50) not null,
  b_id varchar(50) not null,
- qty int(11) not null,
- price DOUBLE(10,2) , 
+ qty int not null,
+ price DOUBLE , 
  chkout varchar(50),
   FOREIGN KEY (c_id) REFERENCES carts(c_id),
   FOREIGN KEY (b_id) REFERENCES books(b_id));
